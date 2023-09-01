e0b82b31083bb5a4868183c0514e4ec10e10fb37
jenikinsdocker <jenikinsdocker@gmail.com>
2023-09-01 23:39:21 +0530

    main class changed

--- src/main/java/com/org/java/SpringbootApplicationDevelopmentApplication.java
+++ src/main/java/com/org/java/SpringbootApplicationDevelopmentApplication.java
@@ -2,9 +2,19 @@ package com.org.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class SpringbootApplicationDevelopmentApplication {
+public class SpringbootApplicationDevelopmentApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SpringbootApplicationDevelopmentApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplicationDevelopmentApplication.class, args);
